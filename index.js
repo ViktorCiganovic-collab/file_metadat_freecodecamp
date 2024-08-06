d79c2c14e78ba8b1a81ec667d5af48a7d72043f8
Viktor Ciganovic <dcifw09@hotmail.se>
2024-08-06 14:46:12 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
+const multer = require('multer');
+const upload = multer();
+const mime = require('mime-types');
 var app = express();
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,9 +16,21 @@ app.get('/', function (req, res) {
 });
 
 
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+const file = req.file;
+
+const fileType = mime.lookup(file);
+
+res.json({
+name: req.file.originalname,
+type: fileType,
+size: req.file,
+})
+
+})
